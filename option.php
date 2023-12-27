8790f4ca0b1fa3093d746c18a67e88fec2a31721
rohman007 <a.nurrohman007@gmail.com>
2023-12-27 16:58:47 +0700

    option

---
+++ option.php
@@ -0,0 +1,176 @@
+
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Poralia">
+        <meta name="generator" content="Poralia">
+        <title>Lowongan - Ini Karir</title>
+
+        <!-- Video.js base CSS -->
+        <link rel="stylesheet" href="https://unpkg.com/video.js@7/dist/video-js.min.css" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,600;0,800;1,400;1,600;1,800&family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="./assets/vendor/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="./assets/vendor/bootstrap/css/bootstrap-utilities.min.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+        <link rel="stylesheet" href="https://aafarma.id/js/tokeninput/token-input.css" type="text/css" />
+        <link rel="stylesheet" href="https://aafarma.id/js/tokeninput/token-input-facebook.css" type="text/css" />
+        <link rel="stylesheet" href="./assets/css/style.css?v=11" />
+
+
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="./assets/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicons/favicon-16x16.png">
+        <link rel="manifest" href="./assets/favicons/site.webmanifest">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+    </head>
+
+    <body class="bg-grey">
+    <div class="d-sm-flex vh-100 align-items-center">
+        <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-12 col-sm-6 col-md-10">
+                <div class="form-block bg-white d-sm-flex rounded-3 py-sm-5 py-4 px-sm-5 px-3 my-3">
+                    <div class="form-block pe-3 ps-2 col-12 col-sm-5 col-md-5 d-none d-sm-flex">
+                        <div class="image-form overflow-hidden rounded-4 d-flex overflow-hidded">
+                            <img src="./assets/images/hero-option.jpg" class="form-image w-100 object-fit-cover">
+                        </div>
+                    </div>
+                    <div class="px-3 col-12 col-sm-7 col-md-6 text-sm ps-sm-5">
+                        <a class="navbar-brand" href="#">
+                            <img src="./assets/images/logo.png" alt="Ini Karir" style="width:55px !important"/>
+                        </a>
+                        <h1 class="fw-700 fs-3 text-black mt-4 mb-3">Hallo Joko,</h1>
+                        <p class="fw-500 mb-5 text-xs">It’s a great day, what will you access this time ?</p>
+                            <a href="#" class="mb-5 button-option d-flex justify-content-between align-items-center text-decoration-none btn btn-primary bg-white text-start text-sm py-3 rounded-4 px-3">
+                                <div class="d-flex align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 50 50" fill="none">
+                                    <circle cx="24.6787" cy="24.6741" r="24.3994" fill="#C4C4C4"/>
+                                    </svg>
+                                    <div class="ms-3">
+                                        <div class="fw-600 text-black">Create Personal Profile</div>
+                                        <div class="text-muted">Joko Purwanto</div>
+                                    </div>
+                                </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M15.2676 4.91418L13.5474 6.63434L20.3549 13.454H5.50781V15.8939H20.3549L13.5474 22.7136L15.2676 24.4337L25.0273 14.674L15.2676 4.91418Z" fill="#898989"/>
+                                </svg>
+                            </a>
+                            <div class="label-login-others text-muted text-center position-relative my-5"><span class="px-2 bg-white text-xs position-relative">Or create</span></div>
+                            
+                    </div>
+                </div>                
+            </div>
+        </div>
+    </div>
+
+
+        <!-- Video.js -->
+        <script src="https://vjs.zencdn.net/8.5.2/video.min.js"></script>
+        
+        <!-- JavaScripts -->
+        <script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+        <script src="./assets/vendor/fullpage/fullpage.min.js"></script>
+        <script src="./assets/js/functions.js"></script>
+        <script>
+            
+            $(document).ready(function() {
+            $(".toggle-password").click(function() {
+
+            $(this).toggleClass("fa-eye fa-eye-slash");
+            var input = $($(this).attr("toggle"));
+            if (input.attr("type") == "password") {
+                input.attr("type", "text");
+            } else {
+                input.attr("type", "password");
+            }
+            });
+            })
+            $(document).ready(function() {
+                $(".button-grid-thumbnail").click(function () {
+                    $(".button-list-thumbnail").removeClass("button-active");
+                    $(".hero-loker-list__wrapper").addClass("grid-thumbnail");
+                    $(this).addClass("button-active");   
+                });
+                $(".button-list-thumbnail").click(function () {
+                    $(".button-grid-thumbnail").removeClass("button-active");
+                    $(".hero-loker-list__wrapper").removeClass("grid-thumbnail");
+                    $(this).addClass("button-active");   
+                });
+            });
+        
+            // Get the tags and input elements from the DOM 
+            const tags = document.getElementById('tags'); 
+            const input = document.getElementById('input-tag'); 
+    
+            // Add an event listener for keydown on the input element 
+            input.addEventListener('keydown', function (event) { 
+    
+                // Check if the key pressed is 'Enter' 
+                if (event.key === 'Enter') { 
+                
+                    // Prevent the default action of the keypress 
+                    // event (submitting the form) 
+                    event.preventDefault(); 
+                
+                    // Create a new list item element for the tag 
+                    const tag = document.createElement('li'); 
+                
+                    // Get the trimmed value of the input element 
+                    const tagContent = input.value.trim(); 
+                
+                    // If the trimmed value is not an empty string 
+                    if (tagContent !== '') { 
+                
+                        // Set the text content of the tag to  
+                        // the trimmed value 
+                        tag.innerText = tagContent; 
+    
+                        // Add a delete button to the tag 
+                        tag.innerHTML += '<button class="delete-button"><img src="assets/images/close-icon.png"></button>'; 
+                        
+                        // Append the tag to the tags list 
+                        tags.appendChild(tag); 
+                        
+                        // Clear the input element's value 
+                        input.value = ''; 
+                    } 
+                } 
+            }); 
+    
+            // Add an event listener for click on the tags list 
+            tags.addEventListener('click', function (event) { 
+    
+                // If the clicked element has the class 'delete-button' 
+                if (event.target.classList.contains('delete-button')) { 
+                
+                    // Remove the parent element (the tag) 
+                    event.target.parentNode.remove(); 
+                } 
+            }); 
+
+            
+            $(function(){
+                $(".suggest-delete").on("click", function(e) {
+                    $(this).parent().remove();
+                });
+                $(".all-delete").on("click", function(e) {
+                    $("#tags li").remove();
+                });
+            });
+        </script> 
+    </body>
+</html>
